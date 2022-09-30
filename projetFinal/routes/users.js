5f2ca07fb16a0b9e37b09db0ff845d16265dcef7
Vincent Belisle <2039855@carrefour.cegepvicto.ca>
2022-09-30 09:32:33 -0400

    Ajout des dépendances, dotenv et mongoose

--- projetFinal/routes/users.js
+++ projetFinal/routes/users.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const Produit = require('../models/produit');
+const { default: mongoose, mongo } = require('mongoose');
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
